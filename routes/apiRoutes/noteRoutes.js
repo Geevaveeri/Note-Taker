50abe6371b25e1f98a703beb51f01fbc408d4f30
Geevaveeri <59940979+Geevaveeri@users.noreply.github.com>
2021-05-22 18:46:21 -0600

    completed storing saved notes and retrieving them

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,17 +1,17 @@
 const router = require("express").Router();
 const { notes } = require("../../db/db.json");
-const { v4: uuidv4 } = require('uuid');
 const createNewNote = require("../../lib/note");
 
 router.get("/notes", (req, res) => {
-    res.json(notes);
+    const results = notes;
+    console.log(results);
+    res.json(results);
 });
 
 router.post("/notes", (req, res) => {
 
     console.log(notes + " notes log before");
-    const newNote = createNewNote(req.body);
-    req.body.id = uuidv4();
+    const newNote = createNewNote(req.body, notes);
     res.json(newNote);
 });
 
